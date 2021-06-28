827ef8067687cf1c62e58b1115b9cb20909a45e4
Oscar Poch <opoch@seuba.com>
2021-06-28 13:21:23 +0200

    target .NET Core 3.1 i self-contained al publicar

---
+++ NORTUA_ImageResizeVB/GifConverter.vb
@@ -0,0 +1,102 @@
+﻿
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.Runtime.InteropServices
+
+Public Class GifConverter
+
+#Region " Public Methods "
+  ' ==========================================================================
+  ' Public Methods
+  ' ==========================================================================
+
+  ' --------------------------------------------------------------------------
+  ' Description: 
+  '   Converts the specified GIF image into a transparent GIF with the
+  '   specified color as transparent.
+  '
+  Public Shared Function GetTransparentGif(ByVal Image As Bitmap, ByVal TransparentColor As Color) As Image
+    Dim w As Integer = Image.Width
+    Dim h As Integer = Image.Height
+    Dim bmp As Bitmap = New Bitmap(w, h, PixelFormat.Format8bppIndexed)
+    Dim originalPalette As ColorPalette = Image.Palette
+    Dim palette As ColorPalette = bmp.Palette
+
+    ' Copy all of the entries from the old palette, removing any
+    ' transparency.
+    For i As Integer = 0 To originalPalette.Entries.Length - 1
+      Dim baseColor As Color = originalPalette.Entries(i)
+      Dim alpha As Integer =
+          CInt(IIf(baseColor.Equals(TransparentColor), 0, 255))
+
+      ' Set the palette entry, setting the transparent color based on
+      ' whether it is the same as the transparent color argument.
+      palette.Entries(i) = Color.FromArgb(alpha, baseColor)
+    Next
+
+    ' Re-insert the palette.
+    bmp.Palette = palette
+
+    ' Transfer the bitmap data from the source image to the new image.
+    TransferBytes(Image, bmp)
+
+    ' Return the new image.
+    Return bmp
+  End Function
+#End Region
+
+#Region " Private Methods "
+  ' ==========================================================================
+  ' Private Methods
+  ' ==========================================================================
+
+  ' --------------------------------------------------------------------------
+  ' Description: 
+  '   Lock the bits of the specified image and return the BitmapData
+  '   structure for the bits locked.
+  '
+  Private Shared Function LockBits(
+      ByVal Image As Bitmap,
+      ByVal LockMode As ImageLockMode
+  ) As BitmapData
+    Return Image.LockBits(
+        New Rectangle(0, 0, Image.Width, Image.Height),
+        LockMode,
+        Image.PixelFormat
+    )
+  End Function
+
+  ' --------------------------------------------------------------------------
+  ' Description: 
+  '   Transfer the bytes of the source image to the destination image.
+  '
+  Private Shared Sub TransferBytes(
+      ByVal SrcImg As Bitmap,
+      ByVal DstImg As Bitmap
+  )
+    Dim srcData As BitmapData = LockBits(SrcImg, ImageLockMode.ReadOnly)
+    Dim dstData As BitmapData = LockBits(DstImg, ImageLockMode.WriteOnly)
+
+    Try
+      For y As Integer = 0 To SrcImg.Height - 1
+        For x As Integer = 0 To SrcImg.Width - 1
+          ' Calculate the offsets of the next byte to read/write.
+          Dim srcOffset As Integer = srcData.Stride * y + x
+          Dim dstOffset As Integer = dstData.Stride * y + x
+
+          ' Read the next byte from the source bitmap.
+          Dim b As Byte = Marshal.ReadByte(srcData.Scan0, srcOffset)
+
+          ' Write the byte to the destination bitmap.
+          Marshal.WriteByte(dstData.Scan0, dstOffset, b)
+        Next x
+      Next y
+    Finally
+      ' Cleanup (unlock the bits of the bitmaps).
+      SrcImg.UnlockBits(srcData)
+      DstImg.UnlockBits(dstData)
+    End Try
+  End Sub
+#End Region
+
+End Class
--- NORTUA_ImageResizeVB/Program.vb
+++ NORTUA_ImageResizeVB/Program.vb
@@ -1,7 +1,141 @@
 Imports System
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Net.Mime.MediaTypeNames
+Imports Image = System.Drawing.Image
 
 Module Program
+
+  Private Structure NameCommandLineStuct
+    Dim Name As String
+    Dim Value As String
+  End Structure
+  Private CommandLineArgs As New List(Of NameCommandLineStuct)
+
+  Dim pathOrig As String
+  Dim pathDest As String
+  Dim Height As Integer
+  Dim Width As Integer
+
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
+
+    Try
+
+      If ParseCommandLine() = False Then Console.WriteLine("usage /ImgSource=" & Chr(34) & "path of the source image" & Chr(34) & " /ImgDest=" & Chr(34) & "path of destination image" & Chr(34) & " /Height=300 /Width=300")
+
+      Dim imgSource As Image = Image.FromFile(pathOrig)
+      Dim imgDest As Image
+      Dim memStream As MemoryStream = New MemoryStream()
+
+      'imgDest = ResizeImage(imgSource, New Size(width:=Width, height:=Height))
+      imgDest = ResizeImage.ResizeImage(imgSource, Width, Height)
+      'imgDest.Save(memStream, ImageFormat.Jpeg)
+      imgDest.Save(pathDest, ImageFormat.Gif)
+
+      Console.WriteLine("OK")
+
+    Catch ex As Exception
+      Console.WriteLine(ex.ToString)
+    End Try
+
   End Sub
+
+  Private Function ParseCommandLine() As Boolean
+
+    Try
+      'step one, Do we have a command line?
+
+      Dim clArgs() As String = Environment.GetCommandLineArgs()
+      Dim Command As String = ""
+
+      For Index As Integer = 1 To clArgs.Count - 1
+        Command = Command & clArgs(Index) & " "
+      Next
+
+      If String.IsNullOrEmpty(Command) Then
+        'give up if we don't
+        Return False
+      End If
+
+      'does the command line have at least one named parameter?
+      If Not Command.Contains("/") Then
+        'give up if we don't
+        Return False
+      End If
+      'Split the command line on our slashes.  
+      Dim Params As String() = Split(Command, "/")
+
+      'Iterate through the parameters passed
+      For Each arg As String In Params
+        'only process if the argument is not empty
+        If Not String.IsNullOrEmpty(arg) Then
+          'and contains an equal 
+          If arg.Contains("=") Then
+
+            Dim tmp As NameCommandLineStuct
+            'find the equal sign
+            Dim idx As Integer = arg.IndexOf("=")
+            'if the equal isn't at the end of the string
+            If idx < arg.Length - 1 Then
+              'parse the name value pair
+              tmp.Name = arg.Substring(0, idx).Trim()
+              tmp.Value = arg.Substring(idx + 1).Trim()
+              Select Case tmp.Name.ToLower
+                Case "imgsource"
+                  pathOrig = tmp.Value
+                Case "imgdest"
+                  pathDest = tmp.Value
+                Case "height"
+                  Height = Val(tmp.Value)
+                Case "width"
+                  Width = Val(tmp.Value)
+                Case Else
+                  Return False
+              End Select
+              'add it to the list.
+              CommandLineArgs.Add(tmp)
+            End If
+          End If
+        End If
+
+      Next
+      Return True
+    Catch ex As Exception
+
+      Console.WriteLine(ex.Message)
+      Return False
+
+    End Try
+
+  End Function
+
+  '#Region " ResizeImage "
+  'Public Function ResizeImage(ByVal image As Image, ByVal size As Size, Optional ByVal preserveAspectRatio As Boolean = True) As Image
+  ' Dim newWidth As Integer
+  'Dim newHeight As Integer
+  'If preserveAspectRatio Then
+  'Dim originalWidth As Integer = image.Width
+  'Dim originalHeight As Integer = image.Height
+  'Dim percentWidth As Single = CSng(size.Width) / CSng(originalWidth)
+  'Dim percentHeight As Single = CSng(size.Height) / CSng(originalHeight)
+  'Dim percent As Single = If(percentHeight < percentWidth,
+  '   percentHeight, percentWidth)
+  '  newWidth = CInt(originalWidth * percent)
+  ' newHeight = CInt(originalHeight * percent)
+  'Else
+  ' newWidth = size.Width
+  'newHeight = size.Height
+  'End If
+  'Dim newImage As Image = New Bitmap(newWidth, newHeight)
+  'Using graphicsHandle As Graphics = Graphics.FromImage(newImage)
+  '   graphicsHandle.InterpolationMode = InterpolationMode.HighQualityBicubic
+  '  graphicsHandle.DrawImage(image, 0, 0, newWidth, newHeight)
+  'End Using
+  'Return newImage
+  'End Function
+
+  '#End Region
+
 End Module
